1e860bf801299ce79c094927dde8e17010b40588
20185087 <20185087@DSN1271158616.cfp127aluno.local>
2021-02-24 14:22:39 -0300

    Ínicio do projeto display 7 segmentos

---
+++ dipaly7seg.X/main.c
@@ -0,0 +1,21 @@
+/*
+ * File:   main.c
+ * Author: 20185087
+ *
+ * Created on 24 de Fevereiro de 2021, 14:08
+ */
+
+#include "delay.h"
+#include "config.h"
+#include <xc.h>
+
+
+void main(void) 
+{
+  
+    while( 1 )
+    {
+    
+    
+    }
+}
